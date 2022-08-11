8cea333c1e0b74c0ba10889dee8498d5fd6a6fc9
celticbay <celticbay@protonmail.com>
2022-08-11 14:20:20 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let btn1 = document.querySelector('#btn-1');
+let btn2 = document.querySelector('#btn-2');
+let btn3 = document.querySelector('#btn-3');
+let btn4 = document.querySelector('#btn-4');
+let btn5 = document.querySelector('#btn-5');
+let btn6 = document.querySelector('#btn-6');
+
+btn1.addEventListener('click',()=>{
+document.body.style.backgroundImage="url('./Images/Background/projecteyeeurope.jpg')";
+});
+
+btn2.addEventListener('click',()=>{
+    document.body.style.backgroundImage="url('./Images/Background/projecteyeeurope.jpg')";
+    });
+
+    btn3.addEventListener('click',()=>{
+        document.body.style.backgroundImage="url('./Images/Background/projecteyeeurope.jpg')";
+        });
+
+        btn4.addEventListener('click',()=>{
+            document.body.style.backgroundImage="url('./Images/Background/projecteyeusacan.jpg')";
+            });
+
+            btn5.addEventListener('click',()=>{
+                document.body.style.backgroundImage="url('./Images/Background/projecteyeusacan.jpg')";
+                });
+
+                btn6.addEventListener('click',()=>{
+                    document.body.style.backgroundImage="url('./Images/Background/projecteyeeurope.jpg')";
+                    });
